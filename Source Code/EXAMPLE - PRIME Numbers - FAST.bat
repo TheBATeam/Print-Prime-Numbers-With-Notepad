cdfd00112d4877fcabb67a4e5a80856a9d41abb8
Ankitamehra93 <48797537+Ankitamehra93@users.noreply.github.com>
2020-05-01 23:28:34 +0530

    First Commit

---
+++ Source Code/EXAMPLE - PRIME Numbers - FAST.bat
@@ -0,0 +1,43 @@
+
+:: As prime Numbers can be divided only by two...numbers_______1 and itself....
+:: so, this property of prime nos. is used in this program...
+
+
+
+@echo off
+cls
+Title Print first 'n' prime Nos.
+setlocal enabledelayedexpansion
+echo.
+set a=2
+set Numcount=1
+:top
+set /p "n=Last Prime no. you want to See ? : "
+if not defined n (goto top)
+::set /a "n=%n%"
+if "%n%" leq "0" (goto error)
+goto calculate_prime
+
+:calculate_prime
+set count=0
+for /l %%x in (!numCount!,1,!n!) do (
+if %%x lss 13 (set me=1) else (Set me=2)
+for /l %%a in (2,1,13) do (
+	set /a remainder=!a!%%%%a
+	if not defined remainder (echo.Error in nested for loop&pause>nul)
+	if /i "!remainder!" == "0" (set /a me+=1)
+	set remainder=
+	)
+if /i "!me!" == "2" (echo.!a!)
+set /a a+=1
+)
+goto end
+
+:end
+pause
+exit /b
+
+:error
+echo. Invalid Entry !!!
+timeout /t "2">nul
+goto top
